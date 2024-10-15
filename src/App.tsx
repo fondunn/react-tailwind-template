6525b1c029979ca7964a14e2b57aeaa10145484c
Denys Karlinskyi <fondunndev@gmail.com>
2024-10-15 17:45:05 +0300

    setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="inline-flex text-2xl font-semibold text-purple-700 lg:text-6xl">
+        Hello React + Tailwind
+      </h1>
+    </div>
+  );
+}
+
+export default App;
